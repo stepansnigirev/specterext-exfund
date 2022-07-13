7efcd97b61277d6152372677961b93d22e1ab901
Stepan Snigirev <snigirev.stepan@gmail.com>
2022-07-13 17:53:40 +0200

    move appconfig to a different file

---
+++ src/cryptoadvance/specterext/exfund/appconfig.py
@@ -0,0 +1,15 @@
+"""
+Here Configuration of your Extension (and maybe your Application) takes place
+"""
+import os
+from cryptoadvance.specter.config import ProductionConfig
+
+class AppProductionConfig(ProductionConfig):
+    """The AppProductionConfig class can be used to user this extension as application"""
+    pass
+    # # Where should the User endup if he hits the root of that domain?
+    # ROOT_URL_REDIRECT = "/spc/ext/exfund"
+    # # I guess this is the only extension which should be available?
+    # EXTENSION_LIST = ["cryptoadvance.specterext.exfund.service"]
+    # # You probably also want a different folder here
+    # SPECTER_DATA_FOLDER = os.path.expanduser("~/.exfund")
--- src/cryptoadvance/specterext/exfund/config.py
+++ src/cryptoadvance/specterext/exfund/config.py
@@ -19,13 +19,3 @@ class ProductionConfig(BaseConfig):
 
     pass
 
-
-class AppProductionConfig(BaseConfig):
-    """The AppProductionConfig class can be used to user this extension as application"""
-    pass
-    # # Where should the User endup if he hits the root of that domain?
-    # ROOT_URL_REDIRECT = "/spc/ext/exfund"
-    # # I guess this is the only extension which should be available?
-    # EXTENSION_LIST = ["cryptoadvance.specterext.exfund.service"]
-    # # You probably also want a different folder here
-    # SPECTER_DATA_FOLDER = os.path.expanduser("~/.exfund")
